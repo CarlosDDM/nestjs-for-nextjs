aef7ca6201ae6099fe1b90c72f29818e157290ba
Carlos Daniel <carlosdanieldiasmendes@gmail.com>
2026-03-25 15:30:38 -0300

    development branch

--- src/common/utils/parse-cors-whitelist.ts
+++ src/common/utils/parse-cors-whitelist.ts
@@ -1,6 +1,6 @@
 export function parseCorsWhitelist(raw: string): string[] {
   return raw
-    .split(/\s+/g)
-    .map(url => url.replace(/\/+$/, ''))
-    .filter(Boolean); // remove strings vazias
+    .split(',')
+    .map(url => url.trim().replace(/\/+$/, ''))
+    .filter(Boolean);
 }
